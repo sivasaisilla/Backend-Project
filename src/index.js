d109fc91a61183b8f69735464776e658649381ae
Sivasai <sivasai3920@gmail.com>
2024-02-17 22:32:09 +0530

    Another better approach to connecting backend

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
-import express from "express"
+dotenv.config();
 
-const app= express();
+connectDB();
 
-(async () => {
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-        
-        app.on("error",error=>{
-          console.log("ERR: ",error);
-          throw error
-        })
+// import express from "express"
 
-        app.listen(process.env.PORT,()=>{
-          console.log(`App is listening on port ${process.env.PORT}`)
-        })
-    } catch (error) {
-        console.error("ERROR: ", error);
-        throw error;
-    }
-})();
+// const app= express();
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+
+//         app.on("error",error=>{
+//           console.log("ERR: ",error);
+//           throw error
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//           console.log(`App is listening on port ${process.env.PORT}`)
+//         })
+//     } catch (error) {
+//         console.error("ERROR: ", error);
+//         throw error;
+//     }
+// })();
